e41a1e543e9663cafdba398fd7ef1037025ce3da
Felix Trost <ga94zux@mytum.de>
2024-02-26 18:06:18 +0100

    fixed cosine schedule

--- forward_process.py
+++ forward_process.py
@@ -83,22 +83,45 @@ class LinScForwardProcess(ForwardProcess):
 class CosScForwardProcess(ForwardProcess):
     def _get_schedule_variables(self, T: int, start: float, end: float):
         f = th.cos(
-            (th.linspace(0, 1, T + 1) + start) / (1 + start) * th.pi / 2
+            (th.linspace(0, 1, T) + start) / (1 + start) * th.pi / 2
         ) ** 2
         cum_alphas = (f / f[0]).reshape(-1, 1, 1, 1)
-        betas = th.min(
-            1 - (cum_alphas[1:] / cum_alphas[:-1]),
+        alphas = th.min(
+            (cum_alphas / th.cat(
+                [th.ones(1, 1, 1, 1), cum_alphas[:-1]],
+                dim=0,
+            )),
             th.tensor(0.999)
         )
-        return betas, 1 - betas, cum_alphas[:-1]
+        betas = 1 - alphas
+        return betas, 1 - betas, cum_alphas
 
 
 if __name__ == '__main__':
-    ls = LinScForwardProcess(1000, 0.0001)
-    cs = CosScForwardProcess(1000, 0.0001)
+    ls = LinScForwardProcess(100, 0.0001)
+    cs = CosScForwardProcess(100, 0.008)
 
     import matplotlib.pyplot as plt
 
-    plt.plot((cs.sqrt_cum_alphas ** 2).squeeze())
-    plt.plot((ls.sqrt_cum_alphas ** 2).squeeze())
+    fig, axs = plt.subplots(2, 2)
+
+    print(ls.betas.squeeze())
+
+    axs[0, 0].plot(ls.betas.squeeze())
+    # axs[0, 0].plot(cs.betas.squeeze())
+    axs[0, 0].set_title("betas")
+
+    axs[0, 1].plot(((ls.sqrt_post_variance ** 2) / ls.betas).squeeze())
+    axs[0, 1].plot(((cs.sqrt_post_variance ** 2) / cs.betas).squeeze())
+    axs[0, 1].set_title("sqrt_alphas")
+
+    axs[1, 0].plot((ls.sqrt_cum_alphas ** 2).squeeze())
+    axs[1, 0].plot((cs.sqrt_cum_alphas ** 2).squeeze())
+    axs[1, 0].set_title("sqrt_cum_alphas")
+
+    axs[1, 1].plot((ls.sqrt_post_variance ** 2).squeeze())
+    # axs[1, 1].plot((cs.sqrt_post_variance ** 2).squeeze())
+    axs[1, 1].set_title("sqrt_post_variance")
+
+    plt.legend(["LinSc", "CosSc"])
     plt.show()
